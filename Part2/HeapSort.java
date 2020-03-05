5676b8143051db6c40ea97111eb72fa4cbe82ea2
yile_xie <xieyl@asiainfo-sec.com>
2020-03-05 09:42:33 +0800

    add heap sort

---
+++ Part2/HeapSort.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+public class HeapSort {
+    private int heapSize = 0;
+    private int swap(int a, int b) {
+        return a;
+    }
+
+    private void maxHeapify(int[] A, int i) {
+        int largest = 0;
+        int l = 2 * i;
+        int r = l + 1;
+
+        if (l < heapSize && A[l] > A[i])
+            largest = l;
+        else
+            largest = i;
+
+        if (r < heapSize && A[r] > A[largest])
+            largest = r;
+
+        if (largest != i) {
+            A[i] = swap(A[largest], A[largest] = A[i]);
+            maxHeapify(A, largest);
+        }
+    }
+
+    private void buildMaxHeap(int[] A) {
+        heapSize = A.length;
+        for (int i = (A.length - 1) >> 1; i >= 0; i--) {
+            maxHeapify(A, i);
+        }
+    }
+
+
+    public void sort(int[] A) {
+        buildMaxHeap(A);
+        System.out.println(Arrays.toString(A));
+        for (int i = A.length - 1; i > 0; i--) {
+            A[i] = swap(A[0], A[0] = A[i]);
+            heapSize--;
+            maxHeapify(A, 0);
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] A = {4, 1, 3, 2, 16, 9,10, 14, 8, 7};
+        System.out.println(Arrays.toString(A));
+        HeapSort ob = new HeapSort();
+        ob.sort(A);
+        System.out.println(Arrays.toString(A));
+    }
+}
